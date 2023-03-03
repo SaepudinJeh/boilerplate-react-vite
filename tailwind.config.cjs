3e0f89a2e963a705422080b5725e823e34f807b3
SaepudinJeh <saepudinjeh@gmail.com>
2023-03-03 21:50:22 +0700

    add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xxs: "330px",
+        xs: "400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+}
