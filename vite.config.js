b2a986fd17aeda80358138399d2acf8366195717
SaepudinJeh <saepudinjeh@gmail.com>
2023-03-03 21:27:09 +0700

    add rule eslint

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), removeConsole()],
-})
+  plugins: [
+    react(),
+    eslint(),
+    removeConsole()
+  ],
+});
